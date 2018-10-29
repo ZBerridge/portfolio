ed25ddbaa2c522f1af89bf725e55bb9391ee2dd5
Zack Berridge <zack.berridge@gmail.com>
2018-10-29 07:38:07 -0400

    updates to the old othello game code
    ok, so looks like the code I dug up was in an unfinished state, which means I'm going to have to dig through it, and see what's breaking.  Good opportunity to add in a simple computer player.

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,111 @@
 <template>
-    <div class="home-logo-container d-flex align-items-center justify-content-center">
+    <div class="othello-game-container">
         <div class="row">
             <div class="col-12">
-                <h4 id="game-logo" class="fade-in">{{ msg }}</h4>
-            </div>
-            <div class="col-12">
                 <figure id="othelloGame">
+                    <table class="table" id="othelloGrid">
+                        <tbody>
+                            <tr>
+                                <th>0</th>
+                                <th>1</th>
+                                <th>2</th>
+                                <th>3</th>
+                                <th>4</th>
+                                <th>5</th>
+                                <th>6</th>
+                                <th>7</th>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(0,0)" class="green"></td>
+                                <td v-on:click="playerTurn(0,1)" class="green"></td>
+                                <td v-on:click="playerTurn(0,2)" class="green"></td>
+                                <td v-on:click="playerTurn(0,3)" class="green"></td>
+                                <td v-on:click="playerTurn(0,4)" class="green"></td>
+                                <td v-on:click="playerTurn(0,5)" class="green"></td>
+                                <td v-on:click="playerTurn(0,6)" class="green"></td>
+                                <td v-on:click="playerTurn(0,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(1,0)" class="green"></td>
+                                <td v-on:click="playerTurn(1,1)" class="green"></td>
+                                <td v-on:click="playerTurn(1,2)" class="green"></td>
+                                <td v-on:click="playerTurn(1,3)" class="green"></td>
+                                <td v-on:click="playerTurn(1,4)" class="green"></td>
+                                <td v-on:click="playerTurn(1,5)" class="green"></td>
+                                <td v-on:click="playerTurn(1,6)" class="green"></td>
+                                <td v-on:click="playerTurn(1,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(2,0)" class="green"></td>
+                                <td v-on:click="playerTurn(2,1)" class="green"></td>
+                                <td v-on:click="playerTurn(2,2)" class="green"></td>
+                                <td v-on:click="playerTurn(2,3)" class="green"></td>
+                                <td v-on:click="playerTurn(2,4)" class="green"></td>
+                                <td v-on:click="playerTurn(2,5)" class="green"></td>
+                                <td v-on:click="playerTurn(2,6)" class="green"></td>
+                                <td v-on:click="playerTurn(2,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(3,0)" class="green"></td>
+                                <td v-on:click="playerTurn(3,1)" class="green"></td>
+                                <td v-on:click="playerTurn(3,2)" class="green"></td>
+                                <td v-on:click="playerTurn(3,3)" class="white"></td>
+                                <td v-on:click="playerTurn(3,4)" class="black"></td>
+                                <td v-on:click="playerTurn(3,5)" class="green"></td>
+                                <td v-on:click="playerTurn(3,6)" class="green"></td>
+                                <td v-on:click="playerTurn(3,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(4,0)" class="green"></td>
+                                <td v-on:click="playerTurn(4,1)" class="green"></td>
+                                <td v-on:click="playerTurn(4,2)" class="green"></td>
+                                <td v-on:click="playerTurn(4,3)" class="black"></td>
+                                <td v-on:click="playerTurn(4,4)" class="white"></td>
+                                <td v-on:click="playerTurn(4,5)" class="green"></td>
+                                <td v-on:click="playerTurn(4,6)" class="green"></td>
+                                <td v-on:click="playerTurn(4,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(5,0)" class="green"></td>
+                                <td v-on:click="playerTurn(5,1)" class="green"></td>
+                                <td v-on:click="playerTurn(5,2)" class="green"></td>
+                                <td v-on:click="playerTurn(5,3)" class="green"></td>
+                                <td v-on:click="playerTurn(5,4)" class="green"></td>
+                                <td v-on:click="playerTurn(5,5)" class="green"></td>
+                                <td v-on:click="playerTurn(5,6)" class="green"></td>
+                                <td v-on:click="playerTurn(5,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(6,0)" class="green"></td>
+                                <td v-on:click="playerTurn(6,1)" class="green"></td>
+                                <td v-on:click="playerTurn(6,2)" class="green"></td>
+                                <td v-on:click="playerTurn(6,3)" class="green"></td>
+                                <td v-on:click="playerTurn(6,4)" class="green"></td>
+                                <td v-on:click="playerTurn(6,5)" class="green"></td>
+                                <td v-on:click="playerTurn(6,6)" class="green"></td>
+                                <td v-on:click="playerTurn(6,7)" class="green"></td>
+                            </tr>
+                            <tr>
+                                <td v-on:click="playerTurn(7,0)" class="green"></td>
+                                <td v-on:click="playerTurn(7,1)" class="green"></td>
+                                <td v-on:click="playerTurn(7,2)" class="green"></td>
+                                <td v-on:click="playerTurn(7,3)" class="green"></td>
+                                <td v-on:click="playerTurn(7,4)" class="green"></td>
+                                <td v-on:click="playerTurn(7,5)" class="green"></td>
+                                <td v-on:click="playerTurn(7,6)" class="green"></td>
+                                <td v-on:click="playerTurn(7,7)" class="green"></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </figure>
-                <button type = "button" id ="newGame">
+                <button v-on:click="newGame" id="newGame">
                     New Game!
                 </button>
             </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <span id="player1score">{{ playerOneScore }}</span>
+            <span id="player2score">{{ playerTwoScore }}</span>
         </div>
     </div>
 </template>
@@ -19,52 +114,18 @@
     export default {
         data () {
             return {
-                msg: '',
-                testMsg : 'Soon to contain an old game I made in JS, Othello.',
                 playerColor: '',
                 oppColor: '',
                 playerNum: 1,
-                gameArray: new Array(8),
-                gameArrayObj: new Object(),
+                gameArray: '',
                 xOrigin: '',
                 yOrigin: '' ,
                 playerOneScore: 0,
-                playerTwoScore: 0,
+                playerTwoScore: 0
             }
         },
         methods: {
-            renderMsg(){
-                this.msg = this.testMsg
-            },
-            Init(){
-                let gameBoard = document.getElementById("othelloGame");
-                let newGame = document.getElementById("newGame");
-                let playerOneScoreBoard = document.getElementById("player1score");
-                let playerTwoScoreBoard = document.getElementById("player2score");
-                if(gameBoard != null) {
-                    gameBoard.innerHTML = this.drawGrid();
-                    console.log(this.drawGrid());
-                } else {
-                    console.log('Game board not found.');
-                }
-                if(newGame  != null){
-                    newGame.onclick = this.newGame();
-                } else {
-                    console.log('New Game Button not found.');
-                }
-                if(playerOneScoreBoard != null) {
-                    playerOneScoreBoard.innerHTML = this.playerOneScore;
-                } else {
-                    console.log('Player 1 scoreboard not found.');
-                }
-                if(playerTwoScoreBoard != null) {
-                    playerTwoScoreBoard.innerHTML = this.playerTwoScore;
-                } else {
-                    console.log('Player 2 scoreboard not found.');
-                }
-                this.gridArray();
-            },
-            gridArray() {
+            gridArray: function() {
                 // creates game board's array.
                 for (let i = 0; i < this.gameArray.length; i++) {
                     this.gameArray[i] = new Array(8);
@@ -75,27 +136,9 @@
                         this.gameArray[row][col] = "green";
                     }
                 }
+                console.log(this.gameArray);
             },
-            drawGrid() {
-                // writes table to HTML document
-                let htmlString = "<table id='othelloGrid'><tr>";
-                for (let i = 0; i < 8; i++) {
-                    htmlString += "<th>" + i + "</th>";
-                }
-                htmlString += "</tr>";
-                for (let j = 0; j < 8; j++) {
-                    htmlString += "<tr>";
-
-                    for (let x = 0; x < 8; x++) {
-                        htmlString += "<td v-on:click=\"playerTurn(" + j + "," + x + ")\"></td>";
-                    }
-                    htmlString += "</tr>";
-                }
-                htmlString += "</table>";
-
-                return htmlString;
-            },
-            refreshGrid() {
+            refreshGrid: function() {
                 // passes through table, matching class of each 'td' element to value of corresponding position in
                 // gameArray.
                 let table = document.getElementById("othelloGrid");
@@ -112,11 +155,10 @@
                         }
                     }
                 }
-
-
             },
-            newGame() {
+            newGame: function() {
                 // intializes game board, setting colors for game start
+                this.gameArray = new Array(8);
                 this.gridArray();
                 this.gameArray[3][3] = "white";
                 this.gameArray[3][4] = "black";
@@ -130,7 +172,7 @@
                 this.oppColor = "black";
 
             },
-            playerTurn(x, y) {
+            playerTurn: function(x, y) {
                 if (this.gameArray[x][y] == "green") {
                     this.xOrigin = eval("x + 0");
                     this.yOrigin = eval("y + 0");
@@ -143,36 +185,30 @@
                     this.validateMove(x, y, 0, 1, this.xOrigin, this.yOrigin);
                     this.validateMove(x, y, 1, 1, this.xOrigin, this.yOrigin);
                 }
-
                 this.nextTurn();
-
-
             },
-            validateMove(x, y, xMove, yMove, xOrigin, yOrigin) {
+            validateMove: function(x, y, xMove, yMove, xOrigin, yOrigin) {
                 let xCo = eval("x + xMove");
                 let yCo = eval("y + yMove");
-                if (this.gameArray[xCo][yCo] === this.oppColor) {
+                if (this.gameArray[xCo][yCo] == this.oppColor) {
                     this.validateMove(xCo, yCo, xMove, yMove, xOrigin, yOrigin);
                 }
-                else if (this.gameArray[xCo][yCo] === this.playerColor) {
+                else if (this.gameArray[xCo][yCo] == this.playerColor) {
                     this.fillMove(xOrigin, yOrigin, xCo, yCo, xMove, yMove);
                 }
 
             },
-            fillMove(xO, yO, xF, yF, xMove, yMove) {
-
-                while ((xO !== xF) || (yO !== yF)) {
+            fillMove: function(xO, yO, xF, yF, xMove, yMove) {
+                while ((xO !== xF) || (yO != yF)) {
                     this.gameArray[xO][yO] = this.playerColor;
                     xO = xO + xMove;
                     yO = yO + yMove;
                 }
-
-
                 this.refreshGrid();
 
             },
-            nextTurn() {
-                if (this.playerColor === "white") {
+            nextTurn: function() {
+                if (this.playerColor == "white") {
                     this.playerColor = "black";
                     this.oppColor = "white";
                 }
@@ -181,10 +217,6 @@
                     this.oppColor = "black";
                 }
             }
-        },
-        mounted(){
-            this.renderMsg(),
-            this.Init()
         }
     }
 </script>
