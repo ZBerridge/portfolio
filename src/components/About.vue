aa950a4d84be49599fd4f423226f510be5205c36
Zack Berridge <zack.berridge@gmail.com>
2019-06-01 17:38:00 -0400

    styling updates
    many changes, work continues on the about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,40 +1,59 @@
 <template>
-    <section id="about" class="py-5 post-wrapper container-fluid fade-in bg-darkgrey">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 font-white" v-html="content">
-                </div>
-            </div>
-        </div>
+    <section id="about" class="py-5 page-wrapper fade-in bg-white">
+        <page-banner :title="post.title" :img_url="post.image_url"></page-banner>
+        <page-content :content="post.content"></page-content>
     </section>
 </template>
 
 <script>
-    import Axios from 'axios'
     import {navCloser} from '../mixins/navCloser'
+    import ApiCalls from '../api/apiServices'
+    import PageBanner from './Page/PageBanner'
+    import PageContent from './Page/PageContent'
 
     export default {
         name: "About",
         mixins: [navCloser],
         data () {
             return {
-                content: '',
+                post: {
+                    title: '',
+                    content: '',
+                    image_url: ''
+                },
                 errors: Array()
             }
         },
         methods: {
             loadContent(){
-                Axios.get('https://api.zberridge.com/wp-json/zb/v1/zb-post?slug=About&full=true').then(response => {
+                /*Axios.get('https://api.zberridge.com/wp-json/zb/v1/zb-post?slug=About&full=true').then(response => {
                     this.content = response.data['post_content']
                 })
                     .catch( e => {
                         this.errors.push(e)
-                    })
+                    })*/
+                let callResult;
+                ApiCalls.getPage("About")
+                    .then(data => {
+                        callResult = data
+                        if (callResult != null) {
+                            this.post.title = callResult.post_title
+                            this.post.content = callResult.post_content
+                            this.post.image_url = callResult.featured_image_url
+                            this.post.date = callResult.post_date
+                        } else {
+                            console.log(callResult.data.errors)
+                        }
+                    }).catch(error => console.log(error))
             }
         },
         mounted() {
             this.closeNav()
             this.loadContent()
+        },
+        components: {
+            'page-banner': PageBanner,
+            'page-content': PageContent
         }
     }
 </script>
---
+++ src/components/Page/PageContent.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "PageContent",
+        props: ['content']
+    }
+</script>
+
